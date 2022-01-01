391006ab635a90806b9f60518ebef120dbacda31
Andrianaforu4u <87538141+Andrianaforu4u@users.noreply.github.com>
2022-01-01 05:44:40 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+import sys
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtWebEngineWidgets import *
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.browser = QWebEngineView()
+        self.browser.setUrl(QUrl('http://powerline.io'))
+        self.setCentralWidget(self.browser)
+        self.showMaximized()
+
+
+
+
+
+
+
+app = QApplication(sys.argv)
+QApplication.setApplicationName('powerline.io')
+window = MainWindow()
+app.exec_()
